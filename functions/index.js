c8aa08e2b1303c593692faae710741ffd5613fdf
Federico Gentile <gentfede+git@gmail.com>
2021-05-28 00:03:41 +0200

    feat: voting (+fixes)

--- functions/index.js
+++ functions/index.js
@@ -9,24 +9,29 @@ const db = admin.firestore();
 exports.initializeUser = functions
     .region('europe-west1')
     .https
-    .onRequest(async (req, res) => {
-      const {userUuid} = req.query;
+    .onCall(async (data, context) => {
+      const {userUuid} = data;
 
-      let batch = db.batch();
+      const userDoc = await db.collection('users').doc(userUuid).get();
+
+      if (userDoc.exists && userDoc.get('initialized')) {
+        return Promise.resolve('USER_ALREADY_INITIALIZED');
+      }
 
       const allCrossings = await db.collection('crossings').get();
 
+      let batch = db.batch();
       let index = 0;
 
       functions.logger.log(`Initializing user ${userUuid}`);
 
       for (const currentCrossing of allCrossings.docs) {
         functions.logger.log(`Working off ${currentCrossing.id}`);
-        batch.update(currentCrossing._ref, {
+        batch.update(currentCrossing.ref, {
           'unseenBy': admin.firestore.FieldValue.arrayUnion(userUuid),
         });
 
-        if ((index + 1) % 499 == 0) {
+        if ((index + 1) % 498 == 0) {
           functions.logger.log(`Committing intermediate batch at index $index`);
           await batch.commit();
           batch = db.batch();
@@ -35,8 +40,83 @@ exports.initializeUser = functions
         index++;
       }
 
+      batch.set(userDoc.ref, {initialized: true}, {merge: true});
+
       functions.logger.log('Committing user eligibility ...');
       await batch.commit();
 
-      res.status(200).send('OK');
+      return Promise.resolve('USER_INITIALIZED');
+    });
+
+exports.vote = functions
+    .region('europe-west1')
+    .https
+    .onCall((data, context) => {
+      const {userUuid, crossingNodeId, vote} = data;
+
+      const voteEnumIndextoString = ['CANT_SAY', 'OK', 'PARKING_CLOSE'];
+      const voteEnumIndextoFirestoreProperty = [
+        'votesNotSure',
+        'votesOk',
+        'votesTooClose',
+      ];
+
+      functions
+          .logger
+          .log(
+              `User ${userUuid} voting for \
+              ${crossingNodeId} with ${vote} \
+              (${voteEnumIndextoString[vote]})`,
+          );
+
+      const crossingRef = db.collection('crossings')
+          .doc(crossingNodeId.split('/')[1]);
+
+      const voteRef = crossingRef
+          .collection('votes')
+          .doc(userUuid);
+
+      const metaDoc = db.collection('meta').doc('meta');
+
+      return db.runTransaction(async (transaction) => {
+        const voteSnapshot = await voteRef.get();
+        const crossingSnapshot = await crossingRef.get();
+        const totalVotesForCrossing = crossingSnapshot.get('votesTotal');
+
+        if (voteSnapshot.exists) {
+          const existingVote = voteSnapshot.get('vote');
+
+          if (existingVote != vote) {
+            transaction.update(crossingRef, {
+              [voteEnumIndextoFirestoreProperty[existingVote]]: admin
+                  .firestore
+                  .FieldValue
+                  .increment(-1),
+              [voteEnumIndextoFirestoreProperty[vote]]: admin
+                  .firestore
+                  .FieldValue
+                  .increment(1),
+            });
+          }
+        } else {
+          transaction.update(crossingRef, {
+            [voteEnumIndextoFirestoreProperty[vote]]: admin
+                .firestore
+                .FieldValue
+                .increment(1),
+            'votesTotal': admin.firestore.FieldValue.increment(1),
+            'unseenBy': admin.firestore.FieldValue.arrayRemove(userUuid),
+          });
+
+          if (totalVotesForCrossing == 4) { // 5th vote was just cast
+            transaction.update(metaDoc, {
+              crossingsWithEnoughVotes: admin.firestore.FieldValue.increment(1),
+            });
+          }
+        }
+
+        transaction.set(voteRef, {vote});
+      })
+          .then((value) => 'Vote cast')
+          .catch((error) => `Failed to cast vote: ${error}`);
     });
